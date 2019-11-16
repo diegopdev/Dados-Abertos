4972bbea39061c28590603f8529886457a944bbd
Diego Pereira de Assis <playskil@gmail.com>
2019-11-16 14:14:37 -0300

    :rocket: react-native web-view set up

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { WebView } from 'react-native-webview';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
     },
 });
 
 function App() {
     return (
         <View style={styles.container}>
-            <Icon name="reorder" size={25} />
-            <Text>APP </Text>
+            <WebView source={{ uri: 'https://google.com' }} />
+            {/* <Icon name="reorder" size={25} />
+            <Text>APP </Text> */}
         </View>
     );
 }
