d1cef3496d393c603f5c61ca13e034a11808980e
Diego Pereira de Assis <playskil@gmail.com>
2019-11-16 14:43:54 -0300

    :rocket: react-native linear-gradient set up

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { WebView } from 'react-native-webview';
+import LinearGradient from 'react-native-linear-gradient';
 
 const styles = StyleSheet.create({
     container: {
@@ -9,15 +10,30 @@ const styles = StyleSheet.create({
         // justifyContent: 'center',
         // alignItems: 'center',
     },
+    linearGradient: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 18,
+        fontFamily: 'Gill Sans',
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
 });
 
 function App() {
     return (
-        <View style={styles.container}>
-            <WebView source={{ uri: 'https://google.com' }} />
-            {/* <Icon name="reorder" size={25} />
-            <Text>APP </Text> */}
-        </View>
+        <LinearGradient
+            colors={['#4c669f', '#3b5998', '#192f6a']}
+            style={styles.linearGradient}
+        >
+            <Text style={styles.buttonText}>Sign in with Facebook</Text>
+        </LinearGradient>
     );
 }
 
